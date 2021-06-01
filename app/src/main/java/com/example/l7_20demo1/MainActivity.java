a3ab25859d3ffea19f5e074f667f787ef0f9e4ab
First Deploy <dm3346@hotmail.com>
2021-06-01 20:27:33 +0700

    first time~

--- app/src/main/java/com/example/l7_20demo1/MainActivity.java
+++ app/src/main/java/com/example/l7_20demo1/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.l7_20demo1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavController controller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controller = Navigation.findNavController(this,R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this, controller);
     }
 }
